3249e920779e802980a358743d7214c0e57954b6
All1in <bm3023068@gmail.com>
2022-07-25 19:55:49 +0300

    started practising Vue.js)
    first app on Vue

---
+++ vue-js/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>My ToDo application</h1>
+    <hr />
+  </div>
+</template>
+
+<script>
+import ToDoList from "@/components/ToDoList";
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
