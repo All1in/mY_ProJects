783e8144c8d8a96116f6ae13e9c6b5130f01ed1d
All1in <bm3023068@gmail.com>
2022-07-31 13:46:51 +0300

    redux state,action, reducer ...

---
+++ redux-start/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import "./App.css"
+import { useDispatch, useSelector } from "react-redux"
+
+function App() {
+  const dispatch = useDispatch()
+  const cash = useSelector(state => state.cash)
+  console.log("cash:", cash);
+
+  const addCash = (cash) => {
+    dispatch({type: "ADD_CASH", payload: cash})
+  }
+
+  const getCash = (cash) => {
+    dispatch({type: "GET_CASH", payload: cash})
+  }
+
+  return (  
+    <div className="App">
+      <div style={{fontSize: "3rem"}}>Cash: {cash}</div>
+        <div style={{display: "flex", gap: "20px"}}>
+            <button className="c-button" onClick={() => addCash(Number(prompt()))}> Поповнити рахунок </button>
+            <button className="c-button" onClick={() => getCash(Number(prompt()))}> Зняти готівку </button>
+        </div> 
+    </div>
+  );
+}
+
+export default App;
